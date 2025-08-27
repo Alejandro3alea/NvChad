2e9693b9ebb7451f5aab34e3bde00065b5adeb0a
Alejandro3alea <abalea@gradiant.org>
2025-08-27 13:00:53 +0200

    Add markdown for setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note: Execute nvim before this script!
-nvim --headless +"TSInstall! python" +"TSInstall! c" +"TSInstall! cpp" +q
+nvim --headless +"TSInstall! python" +"TSInstall! c" +"TSInstall! cpp" +"TSInstall! markdown" +q
 
 # Install VSCode Dark plus theme
 git clone https://github.com/0zean/NvChad-vscodedarkplus ./temp
--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import pandas as pd
 
 
@@ -9,4 +10,11 @@ logger("HELLOO")
 
 
 class Input:
-    time_val: int = 0
+    def __init__(self, key: Optional[int]):
+        self.key_val: int = key or 0
+
+    def set_key(self, key: int):
+        self.key_val = key
+
+    def get_key(self) -> int:
+        return self.key_val
